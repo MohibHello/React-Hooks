6457f2e6d14912f6382e548b014daf074b1e1497
Mohib <www.muhibkhan@gmail.com>
2020-05-24 23:29:39 +0530

    updating nested object logic and description added...

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     {/* <ClassCounter/>
     <FunctionalCounter/> */}
     <StateObject/>
-    <MultipleUseState/>
+    {/* <MultipleUseState/> */}
     </div>
   );
 }
--- src/components/useState/StateObject.js
+++ src/components/useState/StateObject.js
@@ -6,12 +6,52 @@ function StateObject() {
     age: "",
     phone: "",
     address: "",
+    anotherObject: {
+      city: "",
+      pin: "",
+      yetAnotherObject:{
+          streetNo:'',
+          houseNo:''
+      }
+    },
   });
 
   const handleChange = (event) => {
     console.log([event.target.name]);
+
+    //copy remaining attributes {Name:Value} and update the one which event triggers
+    /*
+    ...state will copy all value eg: name:'mohib',age:'23',phone:'5655',address''
+    now u want to change address then it will update update and remaining value remain same not undefined
+    */
     setState({ ...state, [event.target.name]: event.target.value });
   };
+
+
+  //setting state for inner/nested object i.e., anotherObject
+  const handleAnotherObjectChange =(event) =>{
+      //do ...state to copy outer state {N:V}
+      //and for inner object like anotherObject:{ here again copy remaining value by doing ...state.anotherObject
+      // if it has more than one {N:V} and update as shown below }
+      setState(
+          {...state,anotherObject:{
+            ...state.anotherObject,[event.target.name]: event.target.value
+          }}
+      )
+  }
+
+ //setting state for inner-of-inner nested object i.e., yetAnotherObj
+ const handleYetAnotherObjectChange =(event) =>{
+    setState(
+        {...state,anotherObject:{
+            yetAnotherObject:{
+                ...state.anotherObject.yetAnotherObject,[event.target.name]: event.target.value 
+            }
+          }}
+    )
+}
+
+
 
   return (
     <div className="container">
@@ -55,11 +95,42 @@ function StateObject() {
           id="address"
           onChange={handleChange}
         />
+        <input
+          className="form-control mt-2"
+          type="text"
+          name="city"
+          id="city"
+          placeholder='city'
+          onChange={handleAnotherObjectChange}
+        />
+        <input
+          className="form-control mt-2"
+          type="number"
+          name="pin"
+          id="pin"
+          placeholder='pincode'
+          onChange={handleAnotherObjectChange}
+        />
+        <input
+          className="form-control mt-2"
+          type="number"
+          name="streetNo"
+          placeholder='street number'
+          onChange={handleYetAnotherObjectChange}
+        />
+        <input
+          className="form-control mt-2"
+          type="number"
+          name="houseNo"
+          placeholder='House number'
+          onChange={handleYetAnotherObjectChange}
+        />
       </form>
       <div>
         <h3>
-          Hi, Your name is {state.name}, {state.age} Year's old with phone{" "}
-          {state.phone} living in {state.address}
+          Hi, Your name is {state.name}, {state.age} Year's old with phone 
+          {state.phone} living in {state.address} with city {state.anotherObject.city} with pin: {state.anotherObject.pin}
+          with streetNo {state.anotherObject.yetAnotherObject.streetNo} and houseNo is{state.anotherObject.yetAnotherObject.houseNo}
         </h3>
       </div>
     </div>
