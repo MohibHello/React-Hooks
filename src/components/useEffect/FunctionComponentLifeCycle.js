8454ed7bb0c398d8e351ef581e0f3ea22c355b1c
Mohib <www.muhibkhan@gmail.com>
2020-05-25 16:07:33 +0530

    Cleaning Up by Returning a Function from the Effect

--- src/components/useEffect/FunctionComponentLifeCycle.js
+++ src/components/useEffect/FunctionComponentLifeCycle.js
@@ -23,20 +23,44 @@ function FunctionalComponentLifeCycle() {
   //similar to ComponentDidMount()
   //mimic the behaviour by passing anononymous arrow function
   useEffect(() => {
+    //setup Effect
     console.log(
       `im inside useEffect function so the current count is ${count}`
     );
+
+    //ComponentWillUnWount Effect logic in return ()
+    //will be called secondth time, it will be queued 
+    //tear down logic for setup Effect is written here 
+    return () => {
+      console.log("return gets called - erases anything written in setup Effect");
+      //the logic will be queued up and will print prevCount
+      console.log(`the last count was ${count}`);
+    };
   });
 
   return (
     <div>
-    <h1 className='mt-5' style={{textAlign:'center'}}>{count}</h1>
-    <div className='row justify-content-center'>
-    <button className="col-auto btn btn-primary mr-2" onClick={handleIncrement}>Increment</button>
-    <button className="col-auto btn btn-secondary ml-2" onClick={handleDecrement}>Decrement</button>
-    <button className="col-auto btn btn-dark ml-2" onClick={handleReset}>Reset</button>
+      <h1 className="mt-5" style={{ textAlign: "center" }}>
+        {count}
+      </h1>
+      <div className="row justify-content-center">
+        <button
+          className="col-auto btn btn-primary mr-2"
+          onClick={handleIncrement}
+        >
+          Increment
+        </button>
+        <button
+          className="col-auto btn btn-secondary ml-2"
+          onClick={handleDecrement}
+        >
+          Decrement
+        </button>
+        <button className="col-auto btn btn-dark ml-2" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </div>
-</div>
   );
 }
 
